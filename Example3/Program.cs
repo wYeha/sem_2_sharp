21bd2f72173e2eb5378b23825909add3386081a6
wyeha <snovavzloman@mail.ru>
2023-08-24 22:07:25 +0300

    3rd done

---
+++ Example3/Program.cs
@@ -0,0 +1,4 @@
+﻿int day = Convert.ToInt32(Console.ReadLine());
+if (day >=1 && day<=5) Console.WriteLine("Нет");
+else if (day>5 && day<=7) Console.WriteLine("Да");
+else Console.WriteLine("Нет такого дня недели");
